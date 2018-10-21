0bccf7299c445badf2fb287dbc470a9df3349528
andech <7578191@gmail.com>
2018-10-21 22:38:28 +0300

    Init develop

---
+++ Procfile
@@ -0,0 +1,2 @@
+back: bundle exec rails s -p 3001
+front: sh -c 'cd ./frontend/ && npm start'
---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,12 @@
+module Api::V1
+  
+  class UsersController < ApplicationController
+
+    def index
+      @users = User.all
+      render json: @users
+    end
+
+  end
+
+end
