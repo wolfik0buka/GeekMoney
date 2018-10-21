dbfab7300870d2fa6fffe1c6856c048ec4e1fb67
andech <7578191@gmail.com>
2018-10-21 22:38:28 +0300

    Init develop

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  devise_for :users,
+              defaults: { format: :json }, path: '',
+              path_names: {
+                sign_in: 'login',
+                sign_out: 'logout',
+                registration: 'signup'
+              },
+              controllers: {
+                sessions: 'sessions',
+                registrations: 'registrations'
+              }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  namespace :api do
+    namespace :v1 do
+      resources :users
+    end
+  end
 end
