ef91248e7103074f1d4e8c5c16676833df7ac4cb
serserch <serserch@yandex.ru>
2018-10-22 14:21:00 +0300

    Config webpack

---
+++ frontend/src/routes.js
@@ -0,0 +1,10 @@
+//import Main from 'containers/Container';
+/*
+export default [
+    {
+        path: '/login',
+        component: Main,
+        exact: true
+    },
+]
+*/
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,57 +3,55 @@ const HTMLplugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry:{
-    main: path.resolve(__dirname, 'src', 'index.jsx'),
-  }, 
-  output: {
-    path: path.resolve(__dirname, 'dist'), 
-    filename: 'bundle.js'
-  },
-  devServer: {
-    contentBase: "./dist",
-    proxy: {
-        '/api': 'http://localhost:3001'
-    }
-  },
-  module:{
-    rules :[
-      {
-        test:/\.jsx?$/,
-        exclude: /node_modules/,
-        use:{
-          loader:'babel-loader'
-        } 
-      },
-      {
-       test: /\.(sa|sc|c)ss$/,
-       use: [
-         MiniCssExtractPlugin.loader,
-         'css-loader',
-         'postcss-loader',
-         'sass-loader',
-       ],
-      } 
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-    alias:{
-      components: path.resolve(__dirname, 'src', 'Components'),
-      containers: path.resolve(__dirname, 'src', 'Containers'),
-    }
-  },
-  plugins :[
-    new HTMLplugin({
-      template: path.resolve(__dirname, 'src', 'index.html'), 
-      filename: 'index.html'
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css',
-    })
-  ],
-  devServer:{
-    historyApiFallback: true,
-  }
+    entry:{
+        main: path.resolve(__dirname, 'src', 'index.jsx'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: "./dist",
+        proxy: {
+            '/api': 'http://localhost:3001'
+        }
+    },
+    module:{
+        rules :[
+            {
+                test:/\.jsx?$/,
+                exclude: /node_modules/,
+                use:{
+                    loader:'babel-loader'
+                }
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias:{
+            components: path.resolve(__dirname, 'src', 'Components'),
+            containers: path.resolve(__dirname, 'src', 'Containers'),
+        }
+    },
+    plugins :[
+        new HTMLplugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+            filename: 'index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+        })
+    ],
 };
