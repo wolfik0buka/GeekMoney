f8fd0e35086dd083ca17ee6e0204de2319b76d31
serserch <serserch@yandex.ru>
2018-10-29 23:19:26 +0200

    Test connect login signin

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -7,7 +7,35 @@ class Login extends PureComponent {
         super(props);
         this.state = {};
     }
-  
+
+    componentDidMount() {
+        //delete localStorage.userid;
+        const userid = localStorage.getItem("userid");
+        if (userid != 'undefined' && userid != null) {
+            console.log(`Userid: ${userid}`)
+        } else {
+            const obj = {
+                "user": {
+                    "email": "test12@test.ru",
+                    "password": "111111"
+                }
+            };
+            fetch('https://floating-woodland-16538.herokuapp.com/api/v1/signin', {
+                method: 'post',
+                headers: {
+                    'Accept': 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(obj)
+            })
+                .then(res => res.json())
+                .then(res => {
+                    localStorage.userid = res.id;
+                    console.log('Signin ' + localStorage.getItem("userid"));
+                });
+        }
+    }
+
     render() {
         return (
             <div className="login">Login</div>
